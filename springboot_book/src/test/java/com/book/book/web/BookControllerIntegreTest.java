c3eef8af6bc43f22385ed93479e1213d5b54c6a6
Kebzzang <kebinkorea@gmail.com>
2021-03-18 20:17:09 +0900

    JUnit test

--- springboot_book/src/test/java/com/book/book/web/BookControllerIntegreTest.java
+++ springboot_book/src/test/java/com/book/book/web/BookControllerIntegreTest.java
@@ -2,7 +2,21 @@ package com.book.book.web;
 
 //통합 테스트 : 컨트롤러로 전체 스프링 프로젝트를 테스트해보겠다.
 //모든 빈들을 똑같이 IoC에 올리고 테스트 하는 것
+/*
+MOCK는 다른 톰캣으로 테스트
+RANDOM_PORT 실제 톰캣으로 테스트
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.MOCK) //전체 테스트!! 다 메모리에 뜬다
 public class BookControllerIntegreTest {
+    @Autowired
+    private MockMvc mockMvc;
 }
--- springboot_book/src/test/java/com/book/book/web/BookControllerUnitTest.java
+++ springboot_book/src/test/java/com/book/book/web/BookControllerUnitTest.java
@@ -3,5 +3,11 @@ package com.book.book.web;
 //단위 테스트 : 컨트롤러만 테스트 하겠다.
 //컨트롤러 관련 로직만 띄우기 !! Filter, ControllerAdvice 이런거..
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest //
+
 public class BookControllerUnitTest {
 }
